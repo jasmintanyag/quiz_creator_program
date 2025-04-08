6f353bbad52a014c162677b7a058daa1eeffeb85
jasmintanyag <tanyagjasmin@gmail.com>
2025-04-09 04:21:47 +0800

    Added colors

--- quiz_creator_file.py
+++ quiz_creator_file.py
@@ -32,10 +32,10 @@ from colorama import init, Fore, Style
 init(autoreset=True)
 
 def create_quiz_file():
-    print("\n Welcome to Quiz Creation!")
-    print("(Type 'exit' anytime to quit.)\n")
+    print(f"\n{Fore.CYAN} Welcome to Quiz Creation!")
+    print(f"{Fore.YELLOW}(Type 'exit' anytime to quit.)\n")
 
-    category = input("Enter a quiz category: ").lower().strip()
+    category = input(f"{Fore.CYAN}Enter a quiz category: ").lower().strip()
     if not category or category == 'exit':
         return
     
@@ -43,37 +43,37 @@ def create_quiz_file():
     questions = []
 
     while True:
-        questn = input("Enter a question: ")
+        questn = input(f"{Fore.CYAN}Enter a question: ")
         if questn.lower() == 'exit':
             break
 
         options = {}
         for choice in ['a', 'b', 'c', 'd']:
-            answers = input(f"Option {choice}: ")
+            answers = input(f"{Fore.YELLOW}Option {choice}: ")
             if answers.lower() == 'exit':
                 return
             
             options[choice] = answers
 
-        correct_ans = input("Enter the CORRECT answer (a, b, c, d): ").lower().strip()
-        print(f"Received input: {correct_ans}") #debugging line
+        correct_ans = input(f"{Fore.CYAN}Enter the CORRECT answer (a, b, c, d): ").lower().strip()
+        print(f"{Fore.YELLOW}Received input: {correct_ans}") #debugging line
         while correct_ans not in ['a', 'b', 'c', 'd']:
-            correct_ans = input("INVALID. Choose only from (a, b, c, d): ").lower().strip()
+            correct_ans = input(f"{Fore.RED}INVALID. Choose only from (a, b, c, d): ").lower().strip()
 
-        print(f"You entered: {correct_ans}")
+            print(f"{Fore.YELLOW}You entered: {correct_ans}")
 
         questions.append({
             "questions": questn,
             "options": options,
             "correct": correct_ans})
         
-        print("Question added!")
+        print(f"{Fore.GREEN}Question added!")
         
     with open(filename, "a") as f:
         for entry in questions:
             f.write(json.dumps(entry) + "\n")
     
-    print(f"All questions are now saved in {filename} under the {category} category.")
+    print(f"{Fore.GREEN}All questions are now saved in {filename} under the {category} category.")
 
 if __name__ == "__main__":
     create_quiz_file()
