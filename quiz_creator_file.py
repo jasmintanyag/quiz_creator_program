b610ab055258a1148e92357f02e4e4c1088f5bdb
jasmintanyag <tanyagjasmin@gmail.com>
2025-04-09 02:43:00 +0800

    Added pseudocode

---
+++ quiz_creator_file.py
@@ -0,0 +1,25 @@
+# Start quiz creation function
+# Print welcome messages
+# Ask user to input category
+
+# Set file name based on category
+# Initialize empty list for questions
+
+# Loop to input questions
+    # Ask user to input question
+
+    # Initialize dictionary for options
+    # Loop through choices (a, b, c, d)
+        # Ask user to input choices
+
+        # Store the choices in options
+
+    # Ask user to input the correct answer
+    # Check if valid input
+
+    # Add the question and options to the questions list 
+    # Print confirmation message
+
+# Write the question in JSON format
+
+# Print ending message
