7dbd5e9b1556c7fb135ba7528a04daaf9d79ec89
jasmintanyag <tanyagjasmin@gmail.com>
2025-05-03 16:33:10 +0800

    Added pseudocode

---
+++ quiz_creator_play.py
@@ -0,0 +1,87 @@
+# Import needed libraries
+
+# Initialize colorama and pygame
+
+# Define play sound
+    # Load music file
+    # Play music file
+
+# Set score file -> "scores.json"
+# Define load scores
+    # If score file exist
+        # Open file and load data then return dictionary
+    # Else
+        # Return empty dictionary
+
+# Define save scores
+    # Open the file in write mode
+    # Write the scores to json
+
+# Define countdown
+    # Play the starting sound
+    # For each second
+        # Print countdown message
+    # When countdown ends
+        # Overwrite the countdown w/ spaces to clear the line
+
+# Define load questions
+    # Initialize empty set to store questions
+    # If file doesn't exist
+        # Print error message
+    # Open file, read each line
+    # Append it to list
+    # If not valid, print error message
+    # Return
+
+# Define list categories
+    # List all the .txt file
+    # Remove .txt from each file name
+    # Return list of category names
+
+# Define play quiz
+    # Get the list of categories
+    # If no category
+        # Print error message
+    # Print welcome message
+    # Ask user to input name
+    # If name is empty
+        # Print error message
+    # Print list of category
+    # Ask user to choose category
+    # If invalid
+        # Print error message
+    # Load questions  of the selected category
+    # If no valid questions
+        # Print error message
+
+    # Randomize the questions
+    # Set initial score to 0
+    # Start the quiz
+
+    # For every question
+        # Put a divider
+        # Print the question
+        # Print all choices (A, B, C, D)
+        # Ask user their answer
+        # If correct
+            # Play 'correct' sound and display 'correct' message
+        # If wrong
+            # Play 'wrong' sound and display 'wrong' message, then show the correct answer
+    
+    # After answering all questions
+        # Print final score
+        # Load all the existing scores
+        # Add or update the score
+        # Save the score, then print confirmation message
+
+# Define main menu
+    # Create loop
+        # Print the header
+        # Show options [1. Play Quiz, 2. Exit]
+        # Ask user input
+        # If 1, call play quiz
+        # If 2, print goodbye message
+        # Else, print error message
+
+# If script is run directly
+    # Call main menu
