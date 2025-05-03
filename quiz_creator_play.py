57b9c6d23fa6f83f574bba512baa00e86d67192f
jasmintanyag <tanyagjasmin@gmail.com>
2025-05-03 17:38:53 +0800

    Remove .txt in filename

--- quiz_creator_play.py
+++ quiz_creator_play.py
@@ -86,6 +86,7 @@ def load_questions(category):
     # Return list of category names
 
 def list_categories():
+    return [filename[:-4] for filename in os.listdir() if filename.endswith(".txt")]
 
 # Define play quiz
     # Get the list of categories
